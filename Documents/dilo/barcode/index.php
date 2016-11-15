5417d2ec1ada923326918b2884e56f3750387a5e
Suwardi <suwardisaras@gmail.com>
2016-11-15 15:20:41 +0700

    first commit

---
+++ Documents/dilo/barcode/index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>Barcode</title>
+</head>
+<body>
+<form name='Buat Barcode' method='get' action='barcode.php'>
+  <input type="text" id="text" name="text" >
+  <input type="submit" name="submit" >
+</form>
+</body>
+</html>
